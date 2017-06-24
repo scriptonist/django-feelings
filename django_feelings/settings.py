b0ef9996d765d9f2fc2ac4019b6ff12c26de2e22
scriptonist <aravind.techbridge@gmail.com>
2017-06-24 22:06:03 +0530

    Starting users

--- django_feelings/settings.py
+++ django_feelings/settings.py
@@ -122,3 +122,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'),]
 
+#Authentication
+LOGIN_URL = 'users:login'
---
+++ users/urls.py
@@ -0,0 +1,3 @@
+from django.conf.urls import url, include
+
+urlpatterns = [url('^', include('django.contrib.auth.urls'), )]
