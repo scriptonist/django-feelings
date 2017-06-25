6b35eea6646896237b7ed11ae7c591a8324ea7b2
scriptonist <aravind.techbridge@gmail.com>
2017-06-25 21:42:06 +0530

    Feeling form done

--- django_feelings/urls.py
+++ django_feelings/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from users import urls as user_urls
+from thoughts import urls as thought_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^users/', include(user_urls,namespace='users')),
+    url(r'^thoughts/', include(thought_urls,namespace='thoughts')),
     url(r'^$', TemplateView.as_view(template_name='index.html'),name='home'),
 
 
