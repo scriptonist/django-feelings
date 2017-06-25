ca68a4f1dea884bd61d9bc1e3cca04fd40f3a02b
scriptonist <aravind.techbridge@gmail.com>
2017-06-25 09:15:31 +0530

    Login,Logout

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 urlpatterns = [
     url('^dashboard/$',views.dashboard,name='dashboard'),
+    url('^logout/$',views.LogoutView.as_view(),name='logout'),
     url('^', include('django.contrib.auth.urls'))
 ]
